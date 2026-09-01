d282623ac182bb62580fa9f3dceb2b691b7e119b
Ayushman Saha <ayushmansaha.1013@gmail.com>
2026-09-01 20:43:25 +0530

    Update vite.config.js

--- truehire-frontend/vite.config.js
+++ truehire-frontend/vite.config.js
@@ -1,19 +1,24 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  // Load env variables based on mode (development, production, etc.)
+  const env = loadEnv(mode, process.cwd(), "");
 
-  server: {
-    host: "localhost",
-    port: 5175,
+  return {
+    plugins: [react()],
 
-    proxy: {
-      "/api": {
-        target: "http://127.0.0.1:8000",
-        changeOrigin: true,
-        secure: false,
+    server: {
+      host: "localhost",
+      port: 5175,
+
+      proxy: {
+        "/api": {
+          target: env.VITE_API_URL || "https://pusher-eggbeater-undermine.ngrok-free.dev",
+          changeOrigin: true,
+          secure: false,
+        },
       },
     },
-  },
+  };
 });
